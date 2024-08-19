eb85a46b17c394078ea03821e578da4b62be7b6d
Adriano Francisco dos Santos <adrianofsantos@proton.me>
2024-08-19 10:59:22 -0300

    feat: Adicionando novos aliases para o comando ls e descrições dos conjutos de aliases

--- zsh/aliases.zsh
+++ zsh/aliases.zsh
@@ -1,17 +1,29 @@
-# Comandos
+# Comandos para kubernetes
 alias k=kubectl
 #alias kc=kubecolor
-alias ls=lsd
+
+# Aliases para comando `ls`
+alias ls="lsd -g"
+alias l=ls
+alias ll="l --tree --depth 2"
+alias la="ll -a"
+
+# Aliases para simplificar a dgitação
 alias v=nvim
 alias lg=lazygit
+
+# Aliases para comando `git`
 alias g=git
 alias gc="git commit"
 alias gc="git checkout"
 alias ga="git add"
+
 # Suffix extensions
 alias -s txt=nvim
 alias -s md=nvim
 alias -s yaml=nvim
 alias -s yml=nvim
 alias -s json=nvim
+
+# O alias abaixo provoca o comportamento de sempre abrir o script no editor de texto e não a execução dele.
 #alias -s sh=nvim
