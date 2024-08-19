d95a1752a7f74a9d3c677bf23a775b79b1fec8cf
Adriano Francisco dos Santos <adrianofsantos@proton.me>
2024-08-19 10:50:09 -0300

    chore: Adicionando zsh_aliases ao projeto

---
+++ zsh/aliases.zsh
@@ -0,0 +1,17 @@
+# Comandos
+alias k=kubectl
+#alias kc=kubecolor
+alias ls=lsd
+alias v=nvim
+alias lg=lazygit
+alias g=git
+alias gc="git commit"
+alias gc="git checkout"
+alias ga="git add"
+# Suffix extensions
+alias -s txt=nvim
+alias -s md=nvim
+alias -s yaml=nvim
+alias -s yml=nvim
+alias -s json=nvim
+#alias -s sh=nvim
