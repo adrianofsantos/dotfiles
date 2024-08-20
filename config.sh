c68113d75d47f2002fb09a0866d435c9ccd7314c
Adriano Francisco dos Santos <adrianofsantos@Aang.local>
2024-08-19 22:44:52 -0300

    chore: Log de execução

--- config.sh
+++ config.sh
@@ -1,7 +1,7 @@
 #/bin/bash
 
 #TODO Testar se o arquivo ~/.zshrc existe e criar ele caso não exista e adicionar o comando ao final do arquivo `eval "$(starship init zsh)"`
-
+echo "Iniciando configuração dos pacotes de configuração usando o comando \"stow\""
 stow . --adopt -v
 
 zsh/zsh-config.sh
--- zsh/zsh-config.sh
+++ zsh/zsh-config.sh
@@ -1,7 +1,9 @@
 #/bin/bash
 
 #TODO Testar se o arquivo ~/.zshrc existe e criar ele caso não exista e adicionar o comando ao final do arquivo `eval "$(starship init zsh)"`
-
+echo "Executando script criar o ~/.zshrc"
 if [[ ! -f ~/.zshrc ]]; then
   stow . -v --adopt --target=$HOME
+else
+  echo "Arquivo já existe e não precisa ser configurado."
 fi
