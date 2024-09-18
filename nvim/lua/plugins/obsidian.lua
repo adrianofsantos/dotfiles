e5b7cd17f06e959a95c3edd10988f8803e2d69c3
Adriano Francisco dos Santos <adriano@sotnas.net>
2024-09-17 23:18:03 -0300

    chore: Novos plugins em fase de teste

---
+++ nvim/lua/plugins/obsidian.lua
@@ -0,0 +1,37 @@
+return {
+  "epwalsh/obsidian.nvim",
+  version = "*",
+  lazy = true,
+  ft = "markdown",
+  -- Replace the above line with this if you only want to load obsidian.nvim for markdown files in your vault:
+  -- event = {
+  --   -- If you want to use the home shortcut '~' here you need to call 'vim.fn.expand'.
+  --   -- E.g. "BufReadPre " .. vim.fn.expand "~" .. "/my-vault/*.md"
+  --   -- refer to `:h file-pattern` for more examples
+  --   "BufReadPre path/to/my-vault/*.md",
+  --   "BufNewFile path/to/my-vault/*.md",
+  -- },
+  dependencies = {
+    -- Required.
+    "nvim-lua/plenary.nvim",
+
+    -- see below for full list of optional dependencies https://github.com/epwalsh/obsidian.nvim?tab=readme-ov-file#plugin-dependencies
+    "nvim-telescope/telescope.nvim",
+    "hrsh7th/nvim-cmp",
+    "pomo.nvim",
+  },
+  opts = {
+    workspaces = {
+      {
+        name = "personal",
+        path = "~/repos/github/Notes/Vaults/MyLife/",
+      },
+      {
+        name = "amer3",
+        path = "~/repos/github/Notes/Vaults/americanas sa/",
+      },
+    },
+
+    -- see below for full list of options 👇
+  },
+}
