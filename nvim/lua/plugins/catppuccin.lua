5650b0502e5cb51db42545218d9038ead3fd8cf8
Adriano Francisco dos Santos <adriano@sotnas.net>
2025-08-28 11:24:47 -0300

    fix: Erro no bufferline na troca de nome de fução ainda não atualizado do catppuccin

--- nvim/lua/plugins/catppuccin.lua
+++ nvim/lua/plugins/catppuccin.lua
@@ -5,5 +5,20 @@ return {
     opts = {
       colorscheme = "catppuccin-mocha",
     },
+    {
+      "akinsho/bufferline.nvim",
+      optional = true,
+      opts = function(_, opts)
+        if (vim.g.colors_name or ""):find("catppuccin") then
+          local ok, mod = pcall(require, "catppuccin.groups.integrations.bufferline")
+          if ok then
+            local get = mod.get_theme or mod.get
+            if type(get) == "function" then
+              opts.highlights = get({})
+            end
+          end
+        end
+      end,
+    },
   },
 }
